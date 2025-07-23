42099e81e476db004c1513f4cfc3e3fd801566fd
Doniyor <donish1979@gmail.com>
2025-07-23 13:50:08 +0300

    ci-cd corrected included docker

--- staticfiles/habits/paginators.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-
-class HabitPaginator(PageNumberPagination):
-    """
-    Пагинатор для вывода ограниченного количества данных на странице.
-    """
-    page_size = 5
