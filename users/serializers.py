f4dfd91fd7f0c909af845108a71d0529ac2bcbfd
Doniyor <donish1979@gmail.com>
2025-07-25 15:04:16 +0300

    corrected user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework.serializers import ModelSerializer
 
 from users.models import CustomUser
@@ -12,6 +13,10 @@ class UserSerializer(ModelSerializer):
         model = CustomUser
         # fields = ("id", "email", "avatar", "phone_number", "city")
         fields = "__all__"
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
 
 
 class UserSerializerLimited(ModelSerializer):
